dc56fb508efae36d2a79ed9066040adcce38fce1
Fly Dev <dev@fly.io>
2025-07-07 09:24:04 +0000

    fix syntax error by removing invalid apply call

--- lib/anubis_plug.ex
+++ lib/anubis_plug.ex
@@ -1,13 +1,8 @@
-
 defmodule AnubisPlug do
   import Plug.Conn
-  
-  # Policies will be loaded at runtime instead of compile time
-    apply(module, function, [])
-  )
-  
+
   def init(opts), do: opts
-  
+
   def call(conn, _opts) do
     case verify_bot(conn) do
       :allow -> conn
@@ -15,37 +10,40 @@ defmodule AnubisPlug do
       :challenge -> issue_challenge(conn)
     end
   end
-  
+
   defp verify_bot(conn) do
     user_agent = get_req_header(conn, "user-agent") |> List.first() || ""
-    
+
     cond do
       matches_policy?(user_agent, :good) -> :allow
       matches_policy?(user_agent, :bad) -> :block
       true -> :challenge
     end
   end
-  
+
   defp issue_challenge(conn) do
     {nonce, target} = AnubisPlug.Challenge.generate()
-    
+
     conn
     |> put_resp_content_type("application/json")
-    |> send_resp(403, Jason.encode!(%{
-      status: "challenge",
-      nonce: nonce,
-      target: target
-    }))
+    |> send_resp(
+      403,
+      Jason.encode!(%{
+        status: "challenge",
+        nonce: nonce,
+        target: target
+      })
+    )
     |> halt()
   end
-  
+
   defp deny_access(conn) do
     conn
     |> put_resp_content_type("application/json")
     |> send_resp(403, Jason.encode!(%{status: "denied"}))
     |> halt()
   end
-  
+
   defp matches_policy?(user_agent, type) do
     policies = get_policies()
     policy = Map.get(policies, type)
