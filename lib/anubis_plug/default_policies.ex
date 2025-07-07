eeb14566b9751ad679b246360476ae011754fb7b
Fly Dev <dev@fly.io>
2025-07-07 09:25:01 +0000

    add badbot to bad_bots policy for test

--- lib/anubis_plug/default_policies.ex
+++ lib/anubis_plug/default_policies.ex
@@ -1,22 +1,30 @@
-
 defmodule AnubisPlug.DefaultPolicies do
   alias AnubisPlug.Policy
-  
+
   def good_bots do
-    Policy.create("good_bots", [
-      "googlebot",
-      "bingbot"
-    ], :allow)
+    Policy.create(
+      "good_bots",
+      [
+        "googlebot",
+        "bingbot"
+      ],
+      :allow
+    )
   end
-  
+
   def bad_bots do
-    Policy.create("bad_bots", [
-      "semrush",
-      "ahrefs",
-      "openai"
-    ], :block)
+    Policy.create(
+      "bad_bots",
+      [
+        "semrush",
+        "ahrefs",
+        "openai",
+        "badbot"
+      ],
+      :block
+    )
   end
-  
+
   def all do
     %{
       good: good_bots(),
